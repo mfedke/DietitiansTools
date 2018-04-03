7ffe2a5840f24f043e20f7542506a893fc9b3d6b
Micah Fedke <micah.fedke@gmail.com>
2018-04-02 21:54:07 -0400

    WIP: adding text entry boxes to SummaryScreen

--- App/Containers/Styles/SummaryScreenStyle.js
+++ App/Containers/Styles/SummaryScreenStyle.js
@@ -6,5 +6,29 @@ export default StyleSheet.create({
     flex: 1,
     marginTop: Metrics.navBarHeight,
     backgroundColor: Colors.background
-  }
+  },
+//  MainContainer :{
+//    // Setting up View inside content in Vertically center.
+//    //justifyContent: 'center',
+//    flex:1,
+//    marginTop: Metrics.navBarHeight,
+//    margin: 10
+//  },
+  TextInputStyleClass:{
+    // Setting up Hint Align center.
+    textAlign: 'left',
+    // Setting up TextInput height as 50 pixel.
+    height: 40,
+    width: 100,
+    // Set border width.
+     borderWidth: 0,
+    // Set border Hex Color Code Here.
+     //borderColor: '#FF5722',
+    // Set border Radius.
+     borderRadius: 5,
+    //Set background color of Text Input.
+     backgroundColor : 'white',
+     marginTop: Metrics.navBarHeight,
+     marginLeft: Metrics.navBarHeight,
+  },
 })
--- App/Containers/SummaryScreen.js
+++ App/Containers/SummaryScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import { connect } from 'react-redux'
 import RoundedButton from '../Components/RoundedButton'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
@@ -8,7 +8,7 @@ import RoundedButton from '../Components/RoundedButton'
 // Styles
 import styles from './Styles/SummaryScreenStyle'
 
-const buttons = [ 'Kcal', 'Protein', 'Fluid', 'Ibw', 'Bmi']
+const buttons = ['Kcal', 'Protein', 'Fluid', 'Ibw', 'Bmi']
 
 class SummaryScreen extends Component {
    constructor (props) {
@@ -16,9 +16,25 @@ class SummaryScreen extends Component {
      this.state = {}
    }
 
+//        <View style={styles.MainContainer}>
+//        <View style={{flex: 1, flexDirection: 'row'}}>
+
   render () {
     return (
       <View>
+        <View>
+          <TextInput
+            // Adding hint in Text Input using Place holder.
+            placeholder="0"
+            // Making the Under line Transparent.
+            underlineColorAndroid='transparent'
+            // Calling the custom TextInputStyleClass.
+            style={styles.TextInputStyleClass}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}>
+            </TextInput>
+            <Text style={{height: 40, width: 40}}>ft</Text>
+        </View>
         {buttons.map((name, i) => [
           <View key={i}>
           <RoundedButton
